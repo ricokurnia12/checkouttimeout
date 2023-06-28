586c63b3e6b7d8f27d7d801202a55d986fd1c8f3
ricokurnia12 <ricokurnia19@gmail.com>
2023-06-28 17:01:18 +0700

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import Timeout from './timeout2.png';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-header">
+        <div>
+          <h1>Waktu checkout anda telah habis</h1>
+          <h3>silahkan melakukan pendaftaran kembali</h3>
+        </div>
+        <img className="mb-2" src={Timeout} />
+      </div>
     </div>
   );
 }
